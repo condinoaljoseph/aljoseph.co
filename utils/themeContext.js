da9b92997c6c7c66faa94c590bb3a584815cb0e3
Al Joseph <condinoaljoseph@gmail.com>
2021-01-23 18:33:56 +0800

    off dark theme by default

--- utils/themeContext.js
+++ utils/themeContext.js
@@ -1,11 +1,11 @@
 import React, { useState, useContext, createContext } from 'react';
 
-const initialValue = { theme: true };
+const initialValue = { theme: false };
 
 export const ThemeContext = createContext(initialValue);
 
 export default function ThemeProvider({ children }) {
-	const [theme, setTheme] = useState(true);
+	const [theme, setTheme] = useState(false);
 
 	const toggleTheme = () => {
 		setTheme(!theme);

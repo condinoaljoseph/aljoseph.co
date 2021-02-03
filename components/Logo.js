c506d381fc7e8b5ce403ecc7b5d7019bc76654cc
Al Joseph <condinoaljoseph@gmail.com>
2021-02-03 20:34:06 +0800

    :pineapple: web vitals

--- components/Logo.js
+++ components/Logo.js
@@ -4,16 +4,21 @@ import { useRouter } from 'next/router';
 const Logo = () => {
 	const router = useRouter();
 	const isIndex = router.pathname === '/';
-	const className = isIndex
-		? `text-3xl`
-		: `text-2xl text-pink-600 dark:text-pink-300 leading-9`;
 
-	return (
+	return isIndex ? (
 		<h1 className="font-sans font-black">
 			<Link href="/">
-				<a className={className}>aljoseph.co</a>
+				<a className="text-3xl">aljoseph.co</a>
 			</Link>
 		</h1>
+	) : (
+		<h3 className="font-sans font-black">
+			<Link href="/">
+				<a className="text-2xl text-pink-600 dark:text-pink-300 leading-9">
+					aljoseph.co
+				</a>
+			</Link>
+		</h3>
 	);
 };
 

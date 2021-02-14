ebef24b54b4ae856c1bce7959b4b21acabcbe926
Al Joseph <condinoaljoseph@gmail.com>
2021-02-15 07:39:10 +0800

    less max width

--- components/Container.js
+++ components/Container.js
@@ -6,7 +6,7 @@ const Container = ({ children }) => {
 	return (
 		<div className={`${theme ? 'dark' : ''}`}>
 			<div className="bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-200 min-h-screen font-serif">
-				<div className="max-w-3xl mx-auto py-10 px-6">{children}</div>
+				<div className="max-w-2xl mx-auto py-10 px-6">{children}</div>
 			</div>
 		</div>
 	);

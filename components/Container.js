76eb337a0475522924a858dc899d958d6cb8c602
paco <34928425+pacocoursey@users.noreply.github.com>
2021-02-15 13:09:47 -0700

    revert body changes

--- components/Container.js
+++ components/Container.js
@@ -1,9 +1,7 @@
 const Container = ({ children }) => {
 		return (
-		<div>
-			<div className="min-h-screen font-serif">
-				<div className="max-w-2xl mx-auto py-10 px-6">{children}</div>
-			</div>
+		<div className="bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-200 min-h-screen font-serif">
+			<div className="max-w-2xl mx-auto py-10 px-6">{children}</div>
 		</div>
 	);
 };

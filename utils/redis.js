a9cdb8d30155fece95da9af7a3271cc9a00e2db3
Al Joseph <condinoaljoseph@gmail.com>
2021-02-16 20:55:52 +0800

    :key: redis

--- utils/redis.js
+++ utils/redis.js
@@ -4,15 +4,11 @@ const dev = process.env.NODE_ENV !== 'production';
 const redisCredentials = dev
 	? 6379
 	: {
-			host: process.env.REDIS_HOST,
-			port: process.env.REDIS_PORT,
-			password: process.env.PASSWORD
+			url: process.env.REDIS_URL
 	  };
 
 const client = redis.createClient(redisCredentials);
 client.on('error', (err) => console.log(err));
-
-console.log(redisCredentials);
 
 export function getCache(key) {
 	return new Promise((resolve, reject) => {
@@ -22,13 +18,11 @@ export function getCache(key) {
 				return;
 			}
 
-			console.log(data, '@@@@@@@@@@@');
 			resolve(JSON.parse(data));
 		});
 	});
 }
 
 export function setCache(key, value) {
-	console.log(value, '@@@@@@@@@@@');
-	client.setex(key, 3600, JSON.stringify(value));
+	client.set(key, JSON.stringify(value), redis.print);
 }

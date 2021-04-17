a1fce93cedd5ad6e1e85561cdd485abcc9af6cbd
Al Joseph <condinoaljoseph@gmail.com>
2021-04-17 13:52:55 +0800

    added api routes for subscribing

---
+++ pages/api/subscribe.js
@@ -0,0 +1,27 @@
+import mailchimp from '@mailchimp/mailchimp_marketing';
+
+mailchimp.setConfig({
+	apiKey: process.env.MAILCHIMP_API_KEY,
+	server: process.env.MAILCHIMP_API_SERVER
+});
+
+export default async (req, res) => {
+	const { fName, email } = req.body;
+
+	if (!email) {
+		return res.status(400).json({ error: 'Email is required' });
+	}
+
+	try {
+		await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
+			email_address: email,
+			status: 'subscribed',
+			merge_fields: {
+				FNAME: fName
+			}
+		});
+		return res.status(201).json({ error: '' });
+	} catch (error) {
+		return res.status(500).json({ error: error.message || error.toString() });
+	}
+};

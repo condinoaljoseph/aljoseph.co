615e581870270e3056c7a4f163bd2cd253e89b93
Al Joseph <condinoaljoseph@gmail.com>
2021-04-30 15:12:55 +0800

    added rss feed

--- components/Footer.js
+++ components/Footer.js
@@ -2,33 +2,45 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<footer className="mt-20 pt-7 text-lg">
-			<a
-				className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none"
-				href="https://twitter.com/condino_aj"
-				target="_blank"
-				rel="noopener"
-			>
-				twitter
-			</a>{' '}
-			&bull;{' '}
-			<a
-				className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none"
-				href="https://github.com/condinoaljoseph"
-				target="_blank"
-				rel="noopener"
-			>
-				github
-			</a>{' '}
-			&bull;{' '}
-			<a
-				className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none"
-				href="https://stackoverflow.com/users/9043355/elpmid"
-				target="_blank"
-				rel="noopener"
-			>
-				stack overflow
-			</a>
+		<footer className="mt-20 pt-7 text-lg flex justify-between">
+			<div>
+				<a
+					className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none"
+					href="https://twitter.com/condino_aj"
+					target="_blank"
+					rel="noopener"
+				>
+					twitter
+				</a>{' '}
+				&bull;{' '}
+				<a
+					className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none"
+					href="https://github.com/condinoaljoseph"
+					target="_blank"
+					rel="noopener"
+				>
+					github
+				</a>{' '}
+				&bull;{' '}
+				<a
+					className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none justify-self-end"
+					href="https://stackoverflow.com/users/9043355/elpmid"
+					target="_blank"
+					rel="noopener"
+				>
+					stack overflow
+				</a>
+			</div>
+			<div>
+				<a
+					className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none justify-self-end"
+					href="https://aljoseph.co/feed.xml"
+					target="_blank"
+					rel="noopener"
+				>
+					rss
+				</a>
+			</div>
 		</footer>
 	);
 };

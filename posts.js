3f6e2a50c44a92777598b293f1bff816cdea8a1a
Al Joseph <condinoaljoseph@gmail.com>
2021-07-29 21:15:25 +0800

    added new blog

--- posts.js
+++ posts.js
@@ -35,6 +35,13 @@ module.exports = {
 			date: '2021-06-02',
 			slug: 'style-guide',
 			readingTime: 2
+		},
+		{
+			title: 'How I Built My Blog',
+			excerpt: 'An overview of the technologies I used in building this blog.',
+			date: '2021-07-30',
+			slug: 'how-i-built-my-blog',
+			readingTime: 2
 		}
 	]
 };

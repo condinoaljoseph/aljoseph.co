f25f5d0beb634c2f99a6b0ba2966fcef184f4742
Al Joseph Condino <condinoaljoseph@gmail.com>
2022-03-06 21:22:33 +0800

    updated theme toggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,11 @@
-import Image from 'next/image';
 import Logo from './Logo';
-import Toggle from './Toggle';
-import { useTheme } from 'next-themes';
+import { ThemeToggle } from './ThemeToggle'
 
 const Header = () => {
-	const { setTheme, theme } = useTheme();
-
 	return (
-		<header className="flex justify-between items-center mb-10">
+		<header className="flex justify-between relative items-center mb-10">
 			<Logo />
-			{theme === undefined ? null : (
-				<Toggle
-					icons={{
-						checked: (
-							<Image
-								src="/assets/moon.png"
-								width={16}
-								height={16}
-								alt="moon"
-								style={{ pointerEvents: 'none' }}
-							/>
-						),
-						unchecked: (
-							<Image
-								src="/assets/sun.png"
-								width={16}
-								height={16}
-								alt="sun"
-								style={{ pointerEvents: 'none' }}
-							/>
-						)
-					}}
-					checked={theme === 'dark'}
-					onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-				/>
-			)}
+			<ThemeToggle />
 		</header>
 	);
 };

4da0952d0bccdaa25d85c866b3a7d7b26f5d38f3
Al Joseph Condino <condinoaljoseph@gmail.com>
2023-04-01 19:45:16 +0800

    add csp

--- next.config.js
+++ next.config.js
@@ -1,5 +1,42 @@
 const { withContentlayer } = require("next-contentlayer");
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  font-src 'self';  
+`;
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 module.exports = withContentlayer({
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   images: {
@@ -13,5 +50,13 @@ module.exports = withContentlayer({
   },
   experimental: {
     appDir: true,
+  },
+  headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 });
